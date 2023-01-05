7856b5e9ea88ccc30c8a97217b7bf1e79d21a943
RashCD <mrrash91@gmail.com>
2023-01-06 03:13:58 +0800

    remove remote global state provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { createTheme, ThemeProvider } from "@mui/material";
 import { red } from "@mui/material/colors";
 import ModuleA from "./ModuleA";
 import React from "react";
-import RemoteGlobalStateProvider from "./GlobalStateProvider";
 
 const theme = createTheme({
   palette: {
@@ -18,9 +17,7 @@ const theme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <RemoteGlobalStateProvider>
-        <ModuleA />
-      </RemoteGlobalStateProvider>
+      <ModuleA />
     </ThemeProvider>
   );
 }
